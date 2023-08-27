4cae82d09240cfa3ed88651fa2ea23311f24e92a
mtlaee <taufikakbar421@gmail.com>
2023-08-27 18:30:16 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,30 @@
+const getData = () => {
+ return [
+    {
+     id: 1,
+     name: 'Taufik Ganteng Kali',
+     tag: 'taufikgantengabadi',
+     imageUrl: '/images/taufik.jpg',
+   },
+   {
+     id: 2,
+     name: 'Dimas Saputra',
+     tag: 'dimasmds',
+     imageUrl: '/images/dimasmds.jpeg',
+   },
+   {
+     id: 3,
+     name: 'Arif Faizin',
+     tag: 'arifaizin',
+     imageUrl: '/images/arifaizin.jpeg',
+   },
+   {
+     id: 4,
+     name: 'Rahmat Fajri',
+     tag: 'rfajri27',
+     imageUrl: '/images/rfajri27.jpeg',
+   },
+ ];
+}
+ 
+export { getData };
